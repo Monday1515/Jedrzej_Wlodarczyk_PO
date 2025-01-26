e12b10186d4e09eedeee14ece47e401d147d4671
Jędrzej Włodarczyk <jedrzejwlodarczyk@gmail.com>
2025-01-26 13:50:17 +0100

    LAB7_JAVA

---
+++ LAB7_JAVA/zad5.java
@@ -0,0 +1,30 @@
+interface MessageService {
+   void sendMessage(String message);
+}
+
+class EmailService implements MessageService {
+    @Override
+    public void sendMessage(String message){
+        System.out.println("Wysłano wiadomość: " + message);
+    }
+}
+
+class Notification {
+    private MessageService messageService;
+   
+    public Notification(MessageService messageService) {
+       this.messageService = messageService;
+    }
+
+    public void notify(String message) {
+       messageService.sendMessage(mesage);
+    }
+}
+
+public class zad5 {
+    public static void main(String[] args) {
+        MessageService emailService = new EmailService();
+        Notification notification = new Notification(emailService); 
+        notification.notify("Witaj w SOLID!");
+    }
+}
